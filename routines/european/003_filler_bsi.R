13310315d4ec82183cb553dff74126639d84c2ab
Stefano Ungaro <83211415+stungaro@users.noreply.github.com>
2026-09-07 09:29:45 +0200

    Update 003_filler_bsi.R
    fixed bug in the filling of ref_ and counterpart_ areas in liabilities object

--- routines/european/003_filler_bsi.R
+++ routines/european/003_filler_bsi.R
@@ -1,9 +1,9 @@
+
+
 library(MDstats); library(MD3)
 
 # Set data directory
 if (!exists("data_dir")) data_dir = getwd()
-if (!exists("loaded_dir")) loaded_dir = data_dir
-
 
 ## load filled iip bop
 ## split of assets and liabilities of banks
@@ -12,27 +12,19 @@ ll=readRDS(file.path(data_dir,'ll_iip_cps.rds')); gc()
 
 # Load loans data
 
-loans_bsi= readRDS(file.path(loaded_dir, "bsi_loans.rds")); gc()
+loans_bsi= readRDS(file.path(data_dir, "bsi_loans.rds")); gc()
 
-
-dim(loans_bsi)
-###  manual allocation of U5 to EA20 [EA would be including the reference area]
+###  manual allocation of U5 and U2 to EA20 [U2 would be including the reference area]
 dimnames(loans_bsi)$REF_AREA[dimnames(loans_bsi)$REF_AREA=='U5'] ='EA20'
+dimnames(loans_bsi)$REF_AREA[dimnames(loans_bsi)$REF_AREA=='U2'] ='EA20'
 dimnames(loans_bsi)$COUNTERPART_AREA[dimnames(loans_bsi)$COUNTERPART_AREA=='U5'] ='EA20'
+dimnames(loans_bsi)$COUNTERPART_AREA[dimnames(loans_bsi)$COUNTERPART_AREA=='U2'] ='EA20'
 
 dimnames(loans_bsi)[['REF_AREA']] = ccode(dimnames(loans_bsi)[['REF_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 dimnames(loans_bsi)[['COUNTERPART_AREA']] = ccode(dimnames(loans_bsi)[['COUNTERPART_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 
-###  manual allocation of EA to EA20 
-dimnames(loans_bsi)$REF_AREA[dimnames(loans_bsi)$REF_AREA=='U5'] ='EA20'
-dimnames(loans_bsi)$COUNTERPART_AREA[dimnames(loans_bsi)$COUNTERPART_AREA=='U5'] ='EA20'
-
-
-#check
-#Loans
-aa[F4.AT.S12T..LE._T.2022q4.]
-
 #LOANS FILLING
+# the MFI is the creditor, so REF_AREA and REF_SECTOR = S12T are the aa convention already
 # maturity A is = total, F= up to 1 year (short term), K = more than 1 year (long term)
 aa[F4..S12T..._T.., usenames=TRUE, onlyna=TRUE] = loans_bsi[".A....1998q4:"]
 aa[F4..S12T..._O.., usenames=TRUE, onlyna=TRUE] = loans_bsi[".A....1998q4:"]
@@ -40,46 +32,27 @@ aa[F4S..S12T..._T.., usenames=TRUE, onlyna=TRUE] = loans_bsi[".F....1998q4:"]
 aa[F4S..S12T..._O.., usenames=TRUE, onlyna=TRUE] = loans_bsi[".F....1998q4:"]
 aa[F4L..S12T..._T.., usenames=TRUE, onlyna=TRUE] = loans_bsi[".K....1998q4:"]
 aa[F4L..S12T..._O.., usenames=TRUE, onlyna=TRUE] = loans_bsi[".K....1998q4:"]
-#check
-#Loans
-aa["F4.AT.S12T..LE._T.2022q4.W2+EA20+DE"]
 
 # Load deposits data
-deposits_bsi = readRDS(file.path(loaded_dir, "bsi_deposits.rds")); gc()
+deposits_bsi = readRDS(file.path(data_dir, "bsi_deposits.rds")); gc()
 
-
-###  manual allocation of U5 to EA20 
+###  manual allocation of U5 and U2 to EA20
 dimnames(deposits_bsi)$REF_AREA[dimnames(deposits_bsi)$REF_AREA=='U5'] ='EA20'
+dimnames(deposits_bsi)$REF_AREA[dimnames(deposits_bsi)$REF_AREA=='U2'] ='EA20'
 dimnames(deposits_bsi)$COUNTERPART_AREA[dimnames(deposits_bsi)$COUNTERPART_AREA=='U5'] ='EA20'
+dimnames(deposits_bsi)$COUNTERPART_AREA[dimnames(deposits_bsi)$COUNTERPART_AREA=='U2'] ='EA20'
 
-dim(deposits_bsi)
 dimnames(deposits_bsi)[['REF_AREA']] = ccode(dimnames(deposits_bsi)[['REF_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 dimnames(deposits_bsi)[['COUNTERPART_AREA']] = ccode(dimnames(deposits_bsi)[['COUNTERPART_AREA']],2,'iso2m',leaveifNA=TRUE); gc()
 
-#<<<<<<< HEAD
-#=======
-###  manual allocation of EA to EA20 
-dimnames(deposits_bsi)$REF_AREA[dimnames(deposits_bsi)$REF_AREA=='U5'] ='EA20'
-dimnames(deposits_bsi)$COUNTERPART_AREA[dimnames(deposits_bsi)$COUNTERPART_AREA=='U5'] ='EA20'
-
-
-#>>>>>>> f55887bf1cfac16c1298286e6e92e55225f65c0d
+# 006 names the depositor sector REF_SECTOR. In ll the depositor is the creditor,
+# so it becomes COUNTERPART_SECTOR. The two geography dimensions are already in
+# the right roles and are not touched.
 names(dimnames(deposits_bsi))[names(dimnames(deposits_bsi))=="REF_SECTOR"] <- 'COUNTERPART_SECTOR'
-names(dimnames(deposits_bsi))[names(dimnames(deposits_bsi))=="REF_AREA"] <- 'COUNTERPART_AREA2'
-names(dimnames(deposits_bsi))[names(dimnames(deposits_bsi))=="COUNTERPART_AREA"] <- 'REF_AREA'
-names(dimnames(deposits_bsi))[names(dimnames(deposits_bsi))=="COUNTERPART_AREA2"] <- 'COUNTERPART_AREA'
-
-dim(deposits_bsi)
-
-#check
-#Deposits
-ll[F2M.EA20+W2..S12T.LE._T.2022q4.AT]
 
 #DEPOSITS FILLING
 ll[F2M...S12T.._T.., usenames=TRUE, onlyna=TRUE] = deposits_bsi[".A....1998q4:"]
 ll[F2M...S12T.._O.., usenames=TRUE, onlyna=TRUE] = deposits_bsi[".A....1998q4:"]
-ll[F2M.EA20+W2..S12T.LE._T.2022q4.AT]
-
 
 gc()
 saveRDS(aa,file.path(data_dir,'aa_iip_bsi.rds'))
@@ -87,4 +60,3 @@ saveRDS(ll,file.path(data_dir,'ll_iip_bsi.rds'))
 
 saveRDS(aa,file.path(data_dir,'vintages/aa_iip_bsi_' %&% format(Sys.time(),'%F') %&% '_.rds'))
 saveRDS(ll,file.path(data_dir,'vintages/ll_iip_bsi_' %&% format(Sys.time(),'%F') %&% '_.rds'))
-
